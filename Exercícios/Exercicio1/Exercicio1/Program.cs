2ce7bbf7f74e56e5861e8a08eff75c466bc77b30
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-12 18:50:00 -0300

    new solutions

--- Exercícios/Exercicio1/Exercicio1/Program.cs
+++ Exercícios/Exercicio1/Exercicio1/Program.cs
@@ -8,26 +8,21 @@ using System.Threading.Tasks;
 namespace Exercicio1 {
     internal class Program {
         static void Main(string[] args) {
-            string produto1 = "Computador";
-            string produto2 = "Mesa de escritório";
+            string frase = Console.ReadLine();
+            string cor1 = Console.ReadLine();
+            
+            string[] vet = Console.ReadLine().Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
 
-            byte idade = 30;
-            int codigo = 5290;
-            char genero = 'M';
-
-            double preco1 = 2100.0;
-            double preco2 = 650.50;
-            double medida = 53.234567;
-
-            Console.WriteLine("Produtos:");
-            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
-            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
-            Console.WriteLine();
-            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
-            Console.WriteLine();
-            Console.WriteLine($"Medida com oito casas decimais:  {medida:F8}");
-            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
-            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(cor1);
+            Console.WriteLine(vet);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
         }
     }
 }
