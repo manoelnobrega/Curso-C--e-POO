fdf3256cf3ae2b4a476e9f914c3d1d1a0ae812b1
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-13 22:46:01 -0300

    course in progress...

---
+++ Exercícios/EX1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1 {
+    internal class Program {
+        static void Main(string[] args) {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de Escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double preco3 = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medidas com oito casas decimais: {preco3:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {preco3:F3}");
+            Console.WriteLine($"Separador decimal invariant culture: {preco3.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Projetos/Course/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course {
+    internal class Program {
+        static void Main(string[] args) {
+            int n1 = Console.Read();
+
+        }
+    }
+}
