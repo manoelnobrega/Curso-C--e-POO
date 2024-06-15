8f400a7766661f5e82e97e38ae2abaeac93f4e72
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-14 22:17:14 -0300

    Ex2 solved

---
+++ Exercícios/EX2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ult = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2]);
+            Console.WriteLine(nome);
+            Console.WriteLine(num);
+            Console.WriteLine(num2.ToString("F2"));
+            Console.WriteLine(ult);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2"));
+        }
+    }
+}
--- Projetos/Course/Program.cs
+++ Projetos/Course/Program.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Principal;
 
 namespace Course {
     internal class Program {
         static void Main(string[] args) {
-            int n1 = Console.Read();
+            int n1 = int.Parse(Console.ReadLine());
+            char crc = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            string[] linha = Console.ReadLine().Split(' ');
+            string nome = linha[0];
+            char sexo = char.Parse(linha[1]);
+            int idade = int.Parse(linha[2]);
+            double altura = double.Parse(linha[3], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(n1);
+            Console.WriteLine(crc);
+            Console.WriteLine(n2.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine($"{nome} {sexo} {idade} {altura:F2}");
         }
     }
 }
