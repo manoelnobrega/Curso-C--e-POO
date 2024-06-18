e26d687955162b3a4e0ea3d6596bb1ec195c6a74
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-18 19:13:47 -0300

    course in progress

--- Projetos/Course/Program.cs
+++ Projetos/Course/Program.cs
@@ -9,22 +9,19 @@ using System.Security.Principal;
 namespace Course {
     internal class Program {
         static void Main(string[] args) {
-            int n1 = int.Parse(Console.ReadLine());
-            char crc = char.Parse(Console.ReadLine());
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
 
-            string[] linha = Console.ReadLine().Split(' ');
-            string nome = linha[0];
-            char sexo = char.Parse(linha[1]);
-            int idade = int.Parse(linha[2]);
-            double altura = double.Parse(linha[3], CultureInfo.InvariantCulture);
-
-
-            Console.WriteLine("Você digitou:");
-            Console.WriteLine(n1);
-            Console.WriteLine(crc);
-            Console.WriteLine(n2.ToString(CultureInfo.InvariantCulture));
-            Console.WriteLine($"{nome} {sexo} {idade} {altura:F2}");
+            if (hora < 12) {
+                Console.WriteLine("Bom dia");
+            }
+            else if (hora < 18) {
+                Console.WriteLine("Boa tarde");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite");
+            }
         }
     }
 }
