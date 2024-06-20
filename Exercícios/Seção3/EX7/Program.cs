1f134df8d76c5ce153df24d64cba860d3bb31a14
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-20 19:07:50 -0300

    exercise #7 solved

---
+++ Exercícios/Seção3/EX7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX7 {
+    internal class Program {
+        static void Main(string[] args) {
+            int num = int.Parse(Console.ReadLine());
+            if (num < 0) {
+                Console.WriteLine("NEGATIVO");
+            }
+            else {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+        }
+    }
+}
