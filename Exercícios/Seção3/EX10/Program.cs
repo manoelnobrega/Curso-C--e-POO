6a2ccf20642c17ea39de459ada0e1e3e8039d4b6
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-20 19:08:41 -0300

    exercise #10 solved

---
+++ Exercícios/Seção3/EX10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX10 {
+    internal class Program {
+        static void Main(string[] args) {
+            int cod = int.Parse(Console.ReadLine());
+            int qtd = int.Parse(Console.ReadLine());
+            double cq = 4.00;
+            double xs = 4.50;
+            double xb = 5.00;
+            double trr = 2.00;
+            double rf = 1.50;
+            double preco = 0.00;
+            if (cod == 1) {
+                preco = cq * qtd;
+            }
+            else if (cod == 2) { 
+                preco = xs * qtd;
+            }
+            else if (cod == 3) {
+                preco = xb * qtd;
+            }
+            else if (cod == 4) {
+                preco = trr * qtd;
+            }
+            else if (cod == 5) {
+                preco = rf * qtd;
+            }
+            Console.WriteLine($"Total: R$ {preco:F2}");
+        }
+    }
+}
