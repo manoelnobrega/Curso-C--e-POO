a0f87580200938771416b82e015aa7fa21519fda
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-20 19:07:15 -0300

    exercise #8 solved

---
+++ Exercícios/Seção3/EX8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX8 {
+    internal class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a % b == 0 || b % a == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
