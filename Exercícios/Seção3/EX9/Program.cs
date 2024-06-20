a3c226f209c581d6dec315904f163ba94d43fafa
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-20 19:06:38 -0300

    exercise #9 solved

---
+++ Exercícios/Seção3/EX9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX9 {
+    internal class Program {
+        static void Main(string[] args) {
+            int h1 = int.Parse(Console.ReadLine());
+            int h2 = int.Parse(Console.ReadLine());
+            int hora = 0;
+            if (h2 > h1) {
+                hora = h2 - h1;
+            }
+            else if (h2 == h1) {
+                hora = 24;
+            }
+            else {
+                hora = (24 - h1) + h2;
+            }
+            Console.WriteLine($"O JOGO DUROU {hora} HORA(S)");
+        }
+    }
+}
