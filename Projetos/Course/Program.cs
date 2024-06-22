bf7c1ea450ba4f7b6868f9dc915b2a9526b3766d
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-21 23:12:09 -0300

    course in progress

--- Projetos/Course/Program.cs
+++ Projetos/Course/Program.cs
@@ -9,19 +9,28 @@ using System.Security.Principal;
 namespace Course {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Qual a hora atual?");
-            int hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite três números:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
 
-            if (hora < 12) {
-                Console.WriteLine("Bom dia");
+            double resul = Maior(a, b, c);
+            Console.WriteLine($"Maior = {resul}");
+
+        }
+
+        static int Maior(int n1, int n2, int n3) {
+            int m;
+            if (n1 > n2 && n1 > n3) {
+                m = n1;
             }
-            else if (hora < 18) {
-                Console.WriteLine("Boa tarde");
+            else if (n2 > n3) { 
+                m = n2;
             }
-            else
-            {
-                Console.WriteLine("Boa noite");
+            else {
+                m = n3;
             }
+            return m;
         }
     }
 }
