e0e5dd92816fd4411ff423fabadc0e34be4b811e
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-22 02:09:44 -0300

    exercise #13 solved

---
+++ Exercícios/Seção3/EX13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX13 {
+    internal class Program {
+        static void Main(string[] args) {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            while (x != 0 && y != 0) {
+                if (x > 0) {
+                    if (y > 0) {
+                        Console.WriteLine("primeiro");
+                    }
+                    else {
+                        Console.WriteLine("quarto");
+                    }
+                }
+                else {
+                    if (y > 0) {
+                        Console.WriteLine("segundo");
+                    }
+                    else {
+                        Console.WriteLine("terceiro");
+                    }
+                }
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
