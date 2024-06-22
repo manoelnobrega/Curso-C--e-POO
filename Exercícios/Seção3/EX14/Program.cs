a0a7b28881c6c9f8106b80b81c1ec07e72f13850
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-22 02:10:02 -0300

    exercise #14 solved

---
+++ Exercícios/Seção3/EX14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX14 {
+    internal class Program {
+        static void Main(string[] args) {
+            int cod = 0;
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+            while (cod != 4) { 
+                cod = int.Parse(Console.ReadLine());
+                if (cod == 1) {
+                    alcool++;
+                }
+                else if (cod == 2) {
+                    gasolina++;
+                }
+                else if (cod == 3) {
+                    diesel++;
+                }
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine($"Alcool: {alcool}");
+            Console.WriteLine($"Gasolina: {gasolina}");
+            Console.WriteLine($"Diesel: {diesel}");
+        }
+    }
+}
