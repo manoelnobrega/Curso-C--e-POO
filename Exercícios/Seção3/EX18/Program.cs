45842f810d924d4958e55f0ff3d2664d5639afbb
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-24 18:24:29 -0300

    exercise #18 solved

---
+++ Exercícios/Seção3/EX18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX18 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"{i} {Math.Pow(i, 2.0)} {Math.Pow(i, 3.0)}");
+            }
+        }
+    }
+}
