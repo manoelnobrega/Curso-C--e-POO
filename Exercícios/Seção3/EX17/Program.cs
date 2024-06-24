4021b7fe121254284887c819c9fc8efe20ec3274
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-24 18:24:14 -0300

    exercise #17 solved

---
+++ Exercícios/Seção3/EX17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX17 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int fat = 1;
+            for (int i = n;  i > 0; i--) {
+                fat *= i;
+            }
+            Console.WriteLine(fat);
+        }
+    }
+}
