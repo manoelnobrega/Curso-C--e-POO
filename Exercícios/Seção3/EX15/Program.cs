40626f5c570170b3822669d07d29be5226aef997
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-24 18:23:12 -0300

    exercise #15 solved

---
+++ Exercícios/Seção3/EX15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX15 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int contIn = 0;
+            int contOut = 0;
+            for (int i = 1; i <= n; i++) {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    contIn++;
+                }
+                else {
+                    contOut++;
+                }
+            }
+            Console.WriteLine($"{contIn} in");
+            Console.WriteLine($"{contOut} out");
+        }
+    }
+}
