97802012ea3f8014b9aeed782603e4ab09245b2f
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-26 16:17:31 -0300

    exercise #2 solved

---
+++ Exercícios/Seção4/EX2/Funcionario.cs
@@ -0,0 +1,6 @@
+﻿namespace EX2 {
+    internal class Funcionario {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ Exercícios/Seção4/EX2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Funcionario f1, f2;
+            f1 = new Funcionario();
+            f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            f1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = (f1.salario + f2.salario) / 2.00;
+            Console.WriteLine($"Salário médio = {salarioMedio.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
