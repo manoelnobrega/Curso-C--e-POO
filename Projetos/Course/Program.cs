5357fc77cb3a28db4dd44388111b81e85684f4e8
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-06-27 15:06:57 -0300

    course in progress

--- Projetos/Course/Program.cs
+++ Projetos/Course/Program.cs
@@ -14,20 +14,17 @@ namespace Course {
             x = new Triangulo();
             y = new Triangulo();
 
-            Console.Write("Entre com as medidas do triângulo X:");
+            Console.WriteLine("Entre com as medidas do triângulo X:");
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Entre com as medidas do triângulo Y:");
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
-            double px = (x.A + x.B + x.C) / 2;
-            double py = (y.A + y.B + y.C) / 2;
 
-            double areaX = Math.Sqrt(px * (px - x.A) * (px - x.B) * (px - x.C));
-            double areaY = Math.Sqrt(py * (py - y.A) * (py - y.B) * (py - y.C));
+            double areaX = x.Area();
+            double areaY = y.Area();
 
             Console.WriteLine($"Área de X = {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"Área de Y = {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
--- Projetos/Course/Triangulo.cs
+++ Projetos/Course/Triangulo.cs
@@ -1,8 +1,16 @@
-﻿namespace Course {
+﻿using System;
+
+namespace Course {
     internal class Triangulo {
 
         public double A;
         public double B;
         public double C;
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
     }
 }
