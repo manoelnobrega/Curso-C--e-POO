13a6e70e900852173590e6e5d63f48f028e3aa93
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-07-02 13:10:58 -0300

    exercise #4 solved

---
+++ Exercícios/Seção4/EX4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4 {
+    internal class Program {
+        static void Main(string[] args) {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine($"AREA = {ret.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Exercícios/Seção4/EX4/Retangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4 {
+    internal class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() { 
+            return Largura * Altura;
+        }
+        public double Perimetro() {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Largura, 2.0) + Math.Pow(Altura, 2.0));
+        }
+    }
+}
