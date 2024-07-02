c1bfbe36a66499948658b6c305790ab72bf43463
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-07-02 13:11:39 -0300

    exercise #5 solved

---
+++ Exercícios/Seção4/EX5/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX5 {
+    internal class Funcionario {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() { 
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem) {
+            porcentagem = porcentagem / 100;
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem);
+        }
+        public override string ToString() {
+            return $"{Nome}, $ {SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Exercícios/Seção4/EX5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX5 {
+    internal class Program {
+        static void Main(string[] args) {
+            Funcionario dados = new Funcionario();
+
+            Console.Write("Nome: ");
+            dados.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            dados.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            dados.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {dados}");
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            dados.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {dados}");
+        }
+    }
+}
