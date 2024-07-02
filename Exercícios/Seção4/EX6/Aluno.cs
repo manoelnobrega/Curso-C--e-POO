60b090b3ea7629b0d5446da97c60780d945772d3
Manoel Nobrega <manoelnobrega000@gmail.com>
2024-07-02 13:11:58 -0300

    exercise #6 solved

---
+++ Exercícios/Seção4/EX6/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX6 {
+    internal class Aluno {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public void Notas() {
+            Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public override string ToString() {
+            if (NotaFinal() >= 60.0) {
+                return "APROVADO";
+            }
+            else {
+                return $"REPROVADO\nFALTARAM {(60.0 - NotaFinal()).ToString("F2", CultureInfo.InvariantCulture)} PONTOS";
+            }
+        }
+    }
+}
---
+++ Exercícios/Seção4/EX6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX6 {
+    internal class Program {
+        static void Main(string[] args) {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Notas();
+
+            Console.WriteLine($"NOTA FINAL = {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine(aluno);
+        }
+    }
+}
