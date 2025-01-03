a51ee6617d1c5e8442922f47393f4bcc12eaf723
Manoel Nobrega <manoelnobrega000@gmail.com>
2025-01-03 18:33:09 -0300

    Update in Seção4

---
+++ Exercícios/Seção4/EX0/Carro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX0
+{
+    internal class Carro
+    {
+        public string Marca;
+        public string Modelo;
+        public int Ano;
+        public bool Disponivel;
+
+        public void MostrarInformacoes()
+        {
+            Console.WriteLine("Aqui está as informações do Veículo: ");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"MARCA..........................{Marca}");
+            Console.WriteLine($"MODELO.........................{Modelo}");
+            Console.WriteLine($"ANO............................{Ano}");
+            if (Disponivel)
+            {
+                Console.WriteLine($"CONDIÇÃO.......................DISPONÍVEL");
+            }
+            else
+            {
+                Console.WriteLine($"CONDIÇÃO.......................ALUGADO");
+            }
+            Console.WriteLine("-------------------------------------");
+        }
+        public void Alugar()
+        {
+            Disponivel = false;
+        }
+        public void Devolver() 
+        {
+            Disponivel = true;
+        }
+    }
+}
---
+++ Exercícios/Seção4/EX0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro novo = new Carro();
+
+            Console.WriteLine("Digite um novo Carro:");
+            Console.Write("Marca: ");
+            novo.Marca = Console.ReadLine();
+            Console.Write("Modelo: ");
+            novo.Modelo = Console.ReadLine();
+            Console.Write("Ano: ");
+            novo.Ano = int.Parse(Console.ReadLine());
+            novo.Disponivel = true; //Por padrão
+
+            //novo.Alugar();
+            //novo.Devolver();
+
+            novo.MostrarInformacoes();
+
+
+
+        }
+    }
+}
