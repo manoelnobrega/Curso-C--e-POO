9ca3b6eab8d2fc0cde67234329a7615224a9c9fa
Manoel Nobrega <manoelnobrega000@gmail.com>
2025-01-06 16:45:34 -0300

    exercise #7 solved

---
+++ Exercícios/Seção4/EX7/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX7 {
+    internal class ConversorDeMoeda {
+        
+        public static double Iof = 0.06;
+
+        public static double CalculoDolarReal(double cot, double dol) {
+            return (cot * dol) + ((cot * dol) * Iof);
+        }
+    }
+}
---
+++ Exercícios/Seção4/EX7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX7 {
+    internal class Program {
+        static void Main(string[] args) {
+            double cotacao;
+            double dolares;
+            double valorFinal;
+
+            Console.Write("Qual é a cotação do dólar? ");
+            cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            dolares = double.Parse(Console.ReadLine());
+
+            valorFinal = ConversorDeMoeda.CalculoDolarReal(cotacao, dolares);
+            Console.WriteLine($"Valor a ser pago em reais: {valorFinal}");
+
+        }
+    }
+}
