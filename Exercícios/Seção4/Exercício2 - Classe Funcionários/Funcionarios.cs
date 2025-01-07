93629411a8bbb7f139f8ca77c3adc836e4ed4668
Manoel Nobrega <manoelnobrega000@gmail.com>
2025-01-06 22:25:49 -0300

    exercise #2 solved

---
+++ Exercícios/Seção4/Exercício2 - Classe Funcionários/Funcionarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício2___Classe_Funcionários
+{
+    internal class Funcionarios
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ Exercícios/Seção4/Exercício2 - Classe Funcionários/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exercício2___Classe_Funcionários;
+using System;
+using System.Globalization;
+
+namespace Exercício2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios f1 = new Funcionarios();
+            Funcionarios f2 = new Funcionarios();
+            double x;
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (f1.Salario + f2.Salario) / 2;
+
+            Console.WriteLine("Salário médio é de: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
