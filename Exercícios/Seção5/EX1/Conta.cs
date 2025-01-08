64bde94a6dd1753becc627d60488b09b74716b0f
Manoel Nobrega <manoelnobrega000@gmail.com>
2025-01-08 16:08:08 -0300

    exercise #01 in progress...

---
+++ Exercícios/Seção5/EX1/Conta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1 {
+    internal class Conta {
+        private int NumConta;
+        public string Nome;
+        private double Saldo;
+    }
+}
