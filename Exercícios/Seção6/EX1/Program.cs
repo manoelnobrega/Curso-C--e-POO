e88ce227648ff1fef66faf54bb48723f6452214f
Manoel Nobrega <manoelnobrega000@gmail.com>
2025-01-14 17:03:29 -0300

    exercise #01 solved

---
+++ Exercícios/Seção6/EX1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1 {
+    internal class Program {
+        static void Main(string[] args) {
+            int quarto, n, i;
+            string nome, email;
+            Aluguel[] pousada = new Aluguel[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < n; i++) {
+                Console.WriteLine($"Aluguel #{i+1}:");
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                quarto = int.Parse(Console.ReadLine());
+
+                pousada[quarto] = new Aluguel(nome, email);
+
+            }
+
+            Console.WriteLine("Quartos Ocupados:");
+            for (i = 0; i < 10; i++) {
+                if (pousada[i] != null) {
+                    Console.Write($"{i}: ");
+                    Console.WriteLine(pousada[i]);
+                }
+            }
+
+        }
+    }
+}
